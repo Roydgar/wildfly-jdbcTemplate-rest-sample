933203be85c0a821698622868ce5e0319b7a5cb4
Viktor_Gritsaenko <Roydgaryshka@gmail.com>
2018-09-18 11:59:02 +0300

    Add simple api documentation

--- src/main/java/tk/roydgar/wildfly/controller/LoginController.java
+++ src/main/java/tk/roydgar/wildfly/controller/LoginController.java
@@ -1,5 +1,9 @@
 package tk.roydgar.wildfly.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,11 +13,19 @@ import tk.roydgar.wildfly.model.service.LoginService;
 
 @RestController
 @RequestMapping("/myLogin")
+@Api(value = "Login", description = "Logins user in the system")
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
+    @ApiOperation(value = "Login user in the system and returns user's token")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "Login success"),
+                    @ApiResponse(code = 404, message = "User not found")
+            }
+    )
     @PostMapping
     public ResponseEntity<Token> loginUser(@RequestBody LoginForm loginForm) {
         return loginService.login(loginForm);
